57eb4b8c713c7568c51019f297c0e2393883e2fd
Shruti Mittal <shrutimittal@Shrutis-MacBook-Air.local>
2019-06-24 00:02:05 +0530

    Updates

---
+++ precache-manifest.3ad700880adb65b8741273f7c047fc08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f36abba5296d85e943f59e5cb24ba70",
+    "url": "/firstreact/index.html"
+  },
+  {
+    "revision": "af2cb185f1bd0f1570b2",
+    "url": "/firstreact/static/css/2.6469f95e.chunk.css"
+  },
+  {
+    "revision": "8f72c775a632fed03c68",
+    "url": "/firstreact/static/css/main.7bda122d.chunk.css"
+  },
+  {
+    "revision": "af2cb185f1bd0f1570b2",
+    "url": "/firstreact/static/js/2.dd579aff.chunk.js"
+  },
+  {
+    "revision": "8f72c775a632fed03c68",
+    "url": "/firstreact/static/js/main.e4897345.chunk.js"
+  },
+  {
+    "revision": "123392da67e38e09f938",
+    "url": "/firstreact/static/js/runtime~main.a7d87b6e.js"
+  }
+]);
